5376cab0eb1762ee7d86d26c951c1fbccccc6a22
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-03-25 14:22:23 +0200

    Replaced EventListener to "load" and expireDate

--- src/cookies.js
+++ src/cookies.js
@@ -1,24 +1,27 @@
 const cookiesDiv = document.querySelector('#cookies'); 
 
 // check if cookies are already accepted and hide the banner if true
-const cookiesAccepted = document.cookie.includes('cookiesAccepted=true');
+const cookiesAccepted = document.cookie.includes("ookiesAccepted=true");
+
 if (cookiesAccepted) {
-    cookiesDiv.style.display = 'none';
+    cookiesDiv.style.display = "none";
 } else {
-    cookiesDiv.style.display = 'flex';
+    cookiesDiv.style.display = "flex";
 }
 // add event listener for DOMContentLoaded 
-document.addEventListener('DOMContentLoaded', () => {
-  const acceptBtn = document.querySelector('#accept-btn'); 
+document.addEventListener("load", () => {
+  const acceptBtn = document.querySelector("#accept-btn"); 
 
-  acceptBtn.addEventListener('click', () => {
+  acceptBtn.addEventListener("click", () => {
     // calculate the expiration date for the cookies (30 days from now)
-    const expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString();
+    const expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 30);
+    const expires = expireDate.toUTCString();
 
     // set the "cookies_accepted" cookie with an expiration date 30 days from now
-    document.cookie = `cookiesAccepted=true; expires=${expires}; path=/`;
+    document.cookie = "cookiesAccepted=true; expires=${expires}; path=/";
 
     // hide the cookies banner
-    cookiesDiv.style.display = 'none';
+    cookiesDiv.style.display = "none";
   });
 });
